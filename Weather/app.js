796cf34c8b593db36d1dd74042ce6b69447634ed
TheCuccu <hoangvancuong123@gmail.com>
2021-06-05 09:50:01 +0700

    refactor code by using destruturing

--- Weather/app.js
+++ Weather/app.js
@@ -1,18 +1,23 @@
-const request = require('postman-request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('New York', (error, data) => {
-    if (error) {
-        return console.log(error);
-    }
-    // console.log('Error', error)
-    // console.log('Data', data)
-    forecast(data.latitude, data.longtitude, (error, forecastData) => {
+const addres = process.argv[2]
+// console.log(process.argv);
+if (!addres) {
+    console.log('Please provide an address!');
+} else {
+    geocode(addres, (error, { latitude, longtitude, location } = {}) => {
         if (error) {
-            console.log(error);
+            return console.log(error);
         }
-        console.log(data.location);
-        console.log(forecastData);
+        // console.log('Error', error)
+        // console.log('Data', data)
+        forecast(latitude, longtitude, (error, forecastData) => {
+            if (error) {
+                console.log(error);
+            }
+            console.log(location);
+            console.log(forecastData);
+        })
     })
-})
+}
--- Weather/utils/forecast.js
+++ Weather/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('postman-request')
 const forecast = (latitude, longtitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=c056997b95f9f13649f48b0024cc31da&query=' + latitude + ',' + longtitude + '&units=m'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather services.', 'No data')
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location!', 'No data')
         } else {
-            callback('No error founded', `${response.body.location.name}: ${response.body.current.weather_descriptions[0]}. Temperature is now ${response.body.current.temperature}oC and it feels like ${response.body.current.feelslike}oC out side`)
+            callback('No error founded', `${body.current.weather_descriptions[0]}. Temperature is now ${body.current.temperature}oC and it feels like ${body.current.feelslike}oC out side`)
         }
     })
 }
