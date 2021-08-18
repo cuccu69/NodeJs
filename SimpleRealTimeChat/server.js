f4aba37cc32b069052c761b352122a87efa0bf31
TheCuccu <hoangvancuong123@gmail.com>
2021-08-19 00:01:39 +0700

    refactor

--- SimpleRealTimeChat/server.js
+++ SimpleRealTimeChat/server.js
@@ -15,13 +15,29 @@ const Message = mongoose.model('Message',{
   message : String
 })
 
-const dbUrl = 'mongodb+srv://cukcoo:<passwords>@mern.h0ree.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const dbUrl = 'mongodb+srv://cukcoo:yesican@mern.h0ree.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+  console.log('=============== connecting mongo ===============');
+  if (err) throw new Error(err)
+  console.log('mongodb connected');
+})
+
+const server = http.listen(3000, () => {
+  console.log('=============== connecting server ===============');
+  console.log('server is running on port', server.address().port);
+});
 
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
     res.send(messages);
   })
 })
+
 
 app.get('/messages/:user', (req, res) => {
   const user = req.params.user
@@ -30,17 +46,18 @@ app.get('/messages/:user', (req, res) => {
   })
 })
 
+
 app.post('/messages', async (req, res) => {
   try{
     const message = new Message(req.body);
     const savedMessage = await message.save()
-      console.log('============message saved============');
+    console.log('=============== message saved ===============');
     const censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
-      res.sendStatus(200);
+    if(censored)
+      await Message.remove({_id: censored.id})
+    else
+      io.emit('message', req.body);
+    res.sendStatus(200);
   } catch (error) {
     res.sendStatus(500);
     return console.log('error',error);
@@ -48,17 +65,3 @@ app.post('/messages', async (req, res) => {
     console.log('Message Posted')
   }
 })
-
-io.on('connection', () => {
-  console.log('a user is connected')
-})
-
-mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
-  console.log('============connecting server============');
-  console.log('mongodb connected',err);
-})
-
-const server = http.listen(3000, () => {
-  console.log('============connecting port============');
-  console.log('server is running on port', server.address().port);
-});
